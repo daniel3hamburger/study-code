141cf41bac7dce6b99e895e6a45199260fd2b676
daniel.ham <hamburger.danie-it25@it.dhbw-ravensburg.de>
2026-02-06 14:28:56 +0100

    pointer uebung

---
+++ courses/coding-in-C/SolutionDaniel/Uebung/pointer_uebung.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+ int myNumbers[5]= {10,20,30,40,50};
+
+ int* Var2= myNumbers+1;
+ int* Var5= myNumbers+4;
+
+printf(" 1 adress: %p", &Var2);
+printf(" 2 adress: %p \n", &Var5);
+printf("Adress dif : %d",(Var5-Var2));
+
+    return 0;
+}
